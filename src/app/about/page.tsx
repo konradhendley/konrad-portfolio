147585e58b2525fdd815750a1af18302d810aa34
Konrad Hendley <56139655+konradhendley@users.noreply.github.com>
2025-12-26 13:26:08 -0700

    Update job description (#6)
    * 12/16 changes for new role
    
    * changes for new role

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,16 +12,16 @@ export default function AboutSection() {
   
           <div className="space-y-6 text-base sm:text-lg md:text-xl text-gray-700 dark:text-gray-300 leading-relaxed">
             <p>
-              Currently I'm a software developer working at Nextworld, where I focus on building scalable, user-focused ERP applications. My work blends technical and analytical skills and allows me to use my creative side. I enjoy solving complex problems with clean, maintainable code.
+              Currently I'm a software engineer working at Strive Health, where I focus on building and maintaining software to help improve care for patients suffering from kidney disease. My work blends technical and analytical skills and allows me to use my creative side. I find joy in solving complex problems that help improve the lives of others.
             </p>
             <p>
-              I like to spend time outside of work coding as well. Recently, I built <a href="https://www.myMunzey.com" className="text-black hover:text-blue-400 transition">Munzey</a>, an expense tracker web app, using various AWS services — such as Lambda, DynamoDB, Cognito, and a React frontend with visualizations. It’s a project that reflects my passion for financial literacy, cloud technologies, and great user experience.
+              I like to spend time outside of work coding as well. Recently, I built <a href="https://www.myMunzey.com" className="text-black hover:text-blue-400 transition">Munzey</a>, an expense tracker web app, using various AWS services — such as Lambda, S3, DynamoDB, CloudFront and a React frontend with visualizations. It’s a project that reflects my interest in financial literacy and my goal to help others, while also allowing me to expand my technical skills.
             </p>
             <p>
-              Outside of coding, I have a variety of hobbies — I love running, cycling, and spending time outside. I'm also into sports in general, whether it’s watching a game or playing casually. When I’m home, you’ll often find me in the kitchen trying new recipes or reading a good book.
+              Outside of coding, I have a variety of hobbies. I love running, cycling, and spending time outside. I'm also into sports in general, whether it’s watching a game or playing casually. When I’m home, I enjoy being in the kitchen trying new recipes or reading a good book.
             </p>
             <p>
-              Thanks for visiting — feel free to explore my projects or reach out. I'm always open to connecting and collaborating.
+              Thank you for visiting, feel free to explore my projects or reach out. I'm always open to connecting and collaborating.
             </p>
           </div>
         </div>
